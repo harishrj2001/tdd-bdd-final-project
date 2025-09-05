b416e728aa692ea17ad45aed1cc2c7630993a830
harishrj2001 <harishrj2001@gmail.com>
2025-09-05 09:01:16 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -167,6 +167,15 @@ class TestProductRoutes(TestCase):
         """It should not Create a Product with wrong Content-Type"""
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+      #readtestcase   
+      def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 
 
     #Update test
